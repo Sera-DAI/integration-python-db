b2c55e62a016f2db505c604c55be447ac72383cb
Sera-DAI <alexmercer20020@gmail.com>
2026-06-22 16:51:46 -0300

    Included the Dataclasses for the module

--- src/app/models.py
+++ src/app/models.py
@@ -1,6 +1,10 @@
-from app.database import db
+from app.extensions import db
+from dataclasses import dataclass
 
 class User(db.Model):
+    id: int
+    username: str
+    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
