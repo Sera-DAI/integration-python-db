6a5cabb99006fcc8bb15dacbced6e67800b1a76b
Sera-DAI <alexmercer20020@gmail.com>
2026-06-23 16:20:19 -0300

    Added Dataclasses for class User and restructured of the tables

--- src/app/models.py
+++ src/app/models.py
@@ -1,10 +1,19 @@
 from app.extensions import db
 from dataclasses import dataclass
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
-    id: int
-    username: str
+@dataclass
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
     
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
+    id: int = db.Column(db.Integer, primary_key=True)
+    username: str = db.Column(db.String(150), unique=True, nullable=False)
+    
+    password_hash = db.Column(db.String(256), nullable=False)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+        
+    def check_password(self,password):
+        return check_password_hash(self.password_hash, password)
