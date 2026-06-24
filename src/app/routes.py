24eb6031b1885e45643281a22ef72ddd30389381
Sera-DAI <alexmercer20020@gmail.com>
2026-06-24 11:25:45 -0300

    Added the routes logout, dashboard and return_user_id

--- src/app/routes.py
+++ src/app/routes.py
@@ -7,16 +7,17 @@ def load_routes(app):
 
     @app.route('/') 
     def Initial_page():
-        return 'This page being running corretly'
+        return "This page being running correctly"
     
     @app.route('/register', methods=['POST'])
     def register():
         data = request.get_json()
-        if data.get('username') or data.get('password'):
-            return jsonify({'Message': 'Username and password is necessary for authentication'}), 400
+        
+        if not data.get('username') or not data.get('password'):
+            return jsonify({"Message": "Username and password is necessary for authentication"}), 400
         
         if User.query.filter_by(username=data.get('username')).first():
-            return jsonify({'Error': 'User already registered'}), 400
+            return jsonify({"Error": "User already registered"}), 400
         
         new_user = User(username=data.get('username'))
         new_user.set_password(data.get('password'))
@@ -32,13 +33,33 @@ def load_routes(app):
         
         if user and user.check_password(data.get('password')):
             login_user(user)
-            return jsonify({'message': 'Login completed with success', 'user': user})
+            return jsonify({"Message": "Login successful", "user": user.username})
         
-        return jsonify({'error': 'Invalid  credentials'}), 401
+        return jsonify({"Error": "Invalid  credentials"}), 401
     
     @app.route('/return_users', methods=['GET'])
     @login_required
     def return_users():
         users = User.query.all()
-        return jsonify(users), 200
+        return jsonify({"Message": "List of all users registered"}, users), 200
         
+    @app.route('/return_user_id/<int:user_id>', methods=['GET'])
+    @login_required
+    def return_user_id(user_id):
+        return jsonify({
+            "Message": "Requested user",
+            "User": User.query.filter_by(id=user_id).first()
+        })
+        
+    @app.route('/dashboard', methods=['GET'])
+    @login_required
+    def dashboard():
+        return jsonify(current_user)
+    
+    @app.route('/logout', methods=['POST'])
+    @login_required
+    def logout():
+        logout_user()
+        return jsonify({
+            "Message": "Logout successfuly"
+        })
